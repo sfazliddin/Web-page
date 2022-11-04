5fa376df3c83560fcb5ad079a3d9af672b0bdb61
Samariddin <gotenks.ssj499@gmail.com>
2022-11-03 23:38:32 -0400

    made some changes

---
+++ App.js
@@ -0,0 +1,6 @@
+function App(){
+
+    var elements = document.querySelectorAll("button[type=button]")
+                   for (var i = 0, element; element = elements[i++];) {
+                      element.value = (element.value).toUpperCase();}
+}
